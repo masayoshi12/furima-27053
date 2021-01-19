d4e7f9ee5ec9b6d99c8e5c81d2ff3e7871739213
masayoshi12 <miyabigi1222@gmail.com>
2021-01-19 17:11:50 +0900

    rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.name}
-    email                 {Faker::Internet.free_email}
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
-    last_name             {"久保"}
-    last_name_kana        {"クボ"}
-    first_name            {"雅義"}
-    first_name_kana       {"マサヨシ"}
-    birthday              {1986/12/22}
+    password { password }
+    password_confirmation { password }
+    last_name             { '久保' }
+    last_name_kana        { 'クボ' }
+    first_name            { '雅義' }
+    first_name_kana       { 'マサヨシ' }
+    birthday              { 1986 / 12 / 22 }
   end
 end
