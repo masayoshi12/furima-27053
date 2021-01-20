8172f8856da5c3ef360d6c96fa7f280a6494cb46
masayoshi12 <miyabigi1222@gmail.com>
2021-01-20 18:06:30 +0900

    ActiveHash導入　モデル　アソシエーション　バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :state, :fee, :prefecture, :day
+  
   belongs_to :user
   has_one_attached :image
+
+  validates :image,:product_name, :explanation, :price, presence: true
+
+  validates : :category_id, :state_id, :fee_id, :prefecture_id, :day_id, numericality: { other_than: 1 } 
 end
