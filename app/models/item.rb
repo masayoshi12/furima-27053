c9da9123b65d2453efedb3c5eb55fb5c7ed974b6
masayoshi12 <miyabigi1222@gmail.com>
2021-01-21 18:23:25 +0900

    単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,13 @@ class Item < ApplicationRecord
   
   belongs_to :user
   has_one_attached :image
-
-  validates :image,:product_name, :explanation, :price, presence: true
-  
+  with_options presence: true do
+    validates :image
+    validates :product_name
+    validates :explanation 
+    with_options format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality:{ greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999 }
+    end
+  end
   validates :category_id, :state_id, :fee_id, :prefecture_id, :day_id, numericality: { other_than: 1 } 
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    product_name { Faker::Alphanumeric.alphanumeric(number: 30) }
+    explanation { Faker::Alphanumeric.alphanumeric(number: 1000) }
+    category_id { 2 }
+    state_id { 2 }
+    fee_id { 2 }
+    prefecture_id { 2 }
+    day_id { 2 }
+    price {Faker::Number.number(digits: 7) }
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
