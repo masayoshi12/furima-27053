8cd1e4dc6458ad020ab1068abc9ce61f65693842
masayoshi12 <miyabigi1222@gmail.com>
2021-01-23 20:35:09 +0900

    編集画面編集　画面遷移制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :user_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,7 +21,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
   private
@@ -27,5 +39,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :product_name, :explanation, :price, :category_id, :state_id, :fee_id, :prefecture_id,
                                  :day_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
+  def user_confirmation
+    unless  current_user == @item.user
+    redirect_to root_path
+    end
   end
 end
