29fd38ac2e025325bcde77304c03e7cf0883bd4b
masayoshi12 <miyabigi1222@gmail.com>
2021-01-25 15:26:27 +0900

    Formオブジェクト作成　buyersモデル作成

---
+++ app/models/buyer_address.rb
@@ -0,0 +1,19 @@
+class BuyerAddress
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number
+  
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :municipalities
+    validates :house_number
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/ }
+    validates :user_id
+    validates :item_id
+  end
+  validates :prefecture_id, numericality: { other_than: 1 }
+
+  def save
+    buyer = Buyer.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, house_number: house_number, building_name: building_name, phone_number: phone_number, buyer_id: buyer.id)
+  end
+end
